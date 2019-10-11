bdba3a45225d60f2b54b39c882ec29ebb2e7d2ac
taka <taka05061@gamil.com>
2019-10-12 02:29:55 +0900

    update:無限スクロールの実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,55 @@
 <template>
- <div class="top">
-   <Header />
-   <NaviBar />
+  <div class="top">
+    <Header />
+    <NaviBar />
     <div class="container-fluid mt-4">
-      <h2>
-        記事一覧
-      </h2>
+      <h2>記事一覧</h2>
       <div class="border" />
       <ArticleList v-bind:Articledata="posts" />
+      <infinite-loading @infinite="infiniteHandler" />
     </div>
   </div>
 </template>
 
 <script>
-import {createClient} from '~/plugins/contentful.js';
+import { createClient } from "~/plugins/contentful.js";
 
 const client = createClient();
 
+const POSTS_PER_PAGE = 5;
+
 export default {
-  name: 'top',
-  async asyncData ({ env }) {
-    return await client.getEntries({
-      'content_type': env.CF_BLOG_POST_TYPE_ID,
-       order: '-fields.createdAt',
-    }).then(entries => {
-      return {
-        posts: entries.items
-      }
-    }).catch(console.error);
+  name: "top",
+  data() {
+    return {
+      page: 1,
+      posts: []
+    };
+  },
+  methods: {
+    infiniteHandler($state) {
+      return client
+        .getEntries({
+          content_type: process.env.CF_BLOG_POST_TYPE_ID,
+          skip: (this.page - 1) * POSTS_PER_PAGE,
+          limit: POSTS_PER_PAGE,
+          order: "-fields.createdAt"
+        })
+        .then(entries => {
+          if (entries.items.length) {
+            this.page++;
+            this.posts.push(...entries.items);
+            console.log(
+              "this.posts=" + this.posts + "(" + this.posts.length + ")"
+            );
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        })
+        .catch(console.error);
+    }
   }
-}
+};
 </script>
 
