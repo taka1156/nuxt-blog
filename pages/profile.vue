ee32177c81379b862118b027006750a44b4a05c8
taka <tomo05061@gmail.com>
2020-09-02 00:15:55 +0900

    update: profileページ更新

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,85 +3,126 @@
     <div class="container-fluid">
       <h1>Profile</h1>
       <hr />
-      <div class="container mt-3">
-        <img
-          class="mx-auto mb-4 rounded-lg"
-          src="@/assets/img/prof.png"
-          width="30%"
-          height="30%"
-        />
-        <h2 class="font-weight-bolder h4">最近やっていること</h2>
-        <div class="mx-auto text-left">
-          <ul class="list-group">
-            <li class="list-group-item m-2">
-              VueやNuxtについて、作りながら学ぶ。
-            </li>
-            <li class="list-group-item m-2">
-              JavaScript(ES6)について学習。
-            </li>
-            <li class="list-group-item m-2">
-              FireBaseやMongoDBで、NoSQL形式について触ってみる。
-            </li>
-            <li class="list-group-item m-2">
-              PHPでのテスト駆動開発について学ぶ。
-            </li>
-            <div class="mr-5 text-right">&nbsp;等々…</div>
-          </ul>
+
+      <section class="contents">
+        <h2 class="contents__title">自己紹介</h2>
+        <div class="profile__box">
+          <img class="profile__img" src="@/assets/img/prof.png" />
+          <p class="contents__sentence">
+            プログラミングは、大学1年から触り始めC、Java、Python2などを学びました。<br />
+            現在は、大学4年生で、個人的にTypeScriptやPHP(+Laravel)について学習しています。<br />
+          </p>
         </div>
-        <h2 class="font-weight-bolder h4">SNSやGitHub等</h2>
-        <div class="mx-auto text-left">
-          <ul class="list-group">
-            <li
-              class="clickable list-group-item m-2"
-              @click="jump('https://github.com/taka1156')"
-            >
-              <img src="@/assets/img/github.png" class="icon" />
+      </section>
+
+      <section class="contents">
+        <h2 class="contents__title">SNSやGitHub等</h2>
+        <ul class="sns">
+          <li class="sns__box">
+            <a href="https://github.com/taka1156" class="sns__link">
+              <img src="@/assets/img/github.png" class="sns__icon" />
               GitHub
-            </li>
-            <li
-              class="clickable list-group-item m-2"
-              @click="jump('https://qiita.com/taka_1156')"
-            >
-              <img src="@/assets/img/qiita.png" class="icon" />
+            </a>
+          </li>
+          <li class="sns__box">
+            <a href="https://qiita.com/taka_1156" class="sns__link">
+              <img src="@/assets/img/qiita.png" class="sns__icon" />
               Qiita
-            </li>
-            <li
-              class="clickable list-group-item m-2"
-              @click="jump('https://twitter.com/taka_Program')"
-            >
-              <img src="@/assets/img/twitter.png" class="icon" />
+            </a>
+          </li>
+          <li class="sns__box">
+            <a href="https://twitter.com/taka_Program" class="sns__link">
+              <img src="@/assets/img/twitter.png" class="sns__icon" />
               Twitter
-            </li>
-            <li
-              class="clickable list-group-item m-2"
-              @click="jump('https://nuxt-takasportfolio.netlify.app')"
-            >
-              <img src="@/assets/img/prof.png" class="icon" />
-              ポートフォリオサイト
-            </li>
-          </ul>
-        </div>
-      </div>
+            </a>
+          </li>
+          <li class="sns__box">
+            <a href="https://www.taka1156.site" class="sns__link">
+              <img src="@/assets/img/prof.png" class="sns__icon" />
+              PortfolioSite
+            </a>
+          </li>
+        </ul>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Profile',
-  methods: {
-    jump(url) {
-      location.href = url;
-    }
-  }
+  name: 'Profile'
 };
 </script>
 
 <style scoped>
-.icon {
-  width: 35px;
-  height: 35px;
-  border-radius: 35px;
+/* css reset */
+ul,
+li {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  display: block;
+}
+
+/* css reset */
+
+.contents {
+  margin: 1.25em auto 2em;
+}
+
+.contents__title {
+  font-size: 1.25em;
+}
+
+.profile__box {
+  display: flex;
+  justify-content: center;
+  margin: 20px auto;
+  height: 140px;
+  width: 90%;
+  border: 0.5px solid gray;
+  border-radius: 5px;
+}
+
+.profile__img {
+  display: block;
+  margin: 20px auto;
+  height: 70%;
+  width: 20%;
+}
+
+.contents__sentence {
+  margin: 15px auto;
+  padding: 10px;
+  width: 70%;
+  text-align: left;
+}
+
+.sns {
+  display: flex;
+  margin: 20px auto;
+  width: 80%;
+  justify-content: space-between;
+}
+
+.sns__box {
+  display: flex;
+  width: 25%;
+}
+
+.sns__link {
+  display: block;
+  margin: 0 auto;
+}
+
+.sns__icon {
+  display: block;
+  margin: 0 auto;
+  height: 50px;
+  width: 50px;
+  border-radius: 100%;
   border: solid 1px gray;
   background-color: gray;
 }
