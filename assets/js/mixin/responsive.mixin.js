133f462de5fba0efbda5d9162c1a5c4473305e86
taka <tomo05061@gmail.com>
2021-02-13 20:51:15 +0900

    fix: レスポンシブ対応の修正

--- assets/js/mixin/responsive.mixin.js
+++ assets/js/mixin/responsive.mixin.js
@@ -4,9 +4,19 @@ export default {
       isMobile: false
     };
   },
+  created() {
+    // 初回実行
+    this.checkMobile();
+  },
   mounted() {
+    // リサイズを検出して、その度にウィンドウサイズを確認
     window.addEventListener('resize', () => {
-      this.isMobile = window.matchMedia('(max-width:800px)').matches;
+      this.checkMobile();
     });
+  },
+  methods: {
+    checkMobile() {
+      this.isMobile = window.matchMedia('(max-width:800px)').matches;
+    }
   }
 };
