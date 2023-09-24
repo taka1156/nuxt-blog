788801ebbcde0d3725aae3b65572930171865232
taka <tomo05061@gmail.com>
2023-09-24 21:32:53 +0900

    update: marked.jsにspoilerやコンテナ構文を追加

--- utils/marked/index.js
+++ utils/marked/index.js
@@ -10,6 +10,64 @@ marked.setOptions({
   gfm: true
 });
 
+// spoiler
+
+const spoiler = {
+  name: 'spoiler',
+  level: 'inline',
+  start(src) {
+    return src.match(/^::/)?.index;
+  },
+  tokenizer(src) {
+    const spoilerRule = /^::\s+([\s\S]+?)\n([\s\S]+?)(\s*::)/gm;
+    const match = spoilerRule.exec(src);
+    if (match) {
+      return {
+        type: 'spoiler',
+        raw: match[0],
+        summary: match[1].trim(),
+        details: match[2].trim()
+      };
+    }
+    return false;
+  },
+  renderer({ summary, details }) {
+    const parsedDetailsBody = marked.parse(details);
+    return `<details><summary>${summary}</summary>${parsedDetailsBody}</details>`;
+  },
+  childTokens: ['details', 'summary']
+};
+
+// container
+const container = (
+  name = 'info',
+  startRegExp = /^:: info/,
+  spoilerRule = /^:: info ([\s\S]+?)\n([\s\S]+?)(\s*::)/gm,
+  className = 'info'
+) => ({
+  name: name,
+  level: 'inline',
+  start(src) {
+    return src.match(startRegExp)?.index;
+  },
+  tokenizer(src) {
+    const match = spoilerRule.exec(src);
+    if (match) {
+      return {
+        type: name,
+        raw: match[0],
+        summary: match[1].trim(),
+        details: match[2].trim()
+      };
+    }
+    return false;
+  },
+  renderer({ summary, details }) {
+    return `<div class="container ${className}"><p class="${className}-summary">${summary}</p><p class="${className}-details">${details}</p></div>`;
+  },
+  childTokens: ['div', 'p']
+});
+
 // 目次生成
 let index = 0;
 let tocs = [];
@@ -28,7 +86,27 @@ const renderer = {
   }
 };
 
-marked.use({ renderer });
+const containers = [
+  container(
+    'point',
+    /^:: point/,
+    /^:: point ([\s\S]+?)\n([\s\S]+?)(\s*::)/gm,
+    'point'
+  ),
+  container('info', /^:: info/, /^:: info ([\s\S]+?)\n([\s\S]+?)(\s*::)/gm, 'info'),
+  container('warn', /^:: warn/, /^:: warn ([\s\S]+?)\n([\s\S]+?)(\s*::)/gm, 'warn'),
+  container(
+    'alert',
+    /^:: alert/,
+    /^:: alert ([\s\S]+?)\n([\s\S]+?)(\s*::)/gm,
+    'alert'
+  )
+];
+
+marked.use({
+  renderer,
+  extensions: [spoiler, ...containers]
+});
 
 const markedWrap = md => {
   // 初期化
